8a11339b79eba101f5d3b3e84c3bc0ec451b69b5
Garrett112 <gww2075@email.vccs.edu>
2024-02-06 14:50:38 -0500

    commit

--- extPersonType.h
+++ extPersonType.h
@@ -7,47 +7,47 @@
 
 class extPersonType :personType {
 public:
-	extPersonType() {
-		phoneNumber = "";
-		relationship = "";
+	extPersonType() {														//constructor
+		phoneNumber = "";													//sets default phoneNumber as  ""
+		relationship = "";													//sets default relationship as ""
 	}
 	extPersonType(std::string F, std::string L, int M, int D, int Y, std::string A, std::string C, std::string S, int Z, std::string P, std::string R) {
-		setFirstName(F);
-		setLastName(L);
-		address.setAddress(A);
-		address.setCity(C);
-		address.setState(S);
-		address.setZipcode(Z);
-		birthday.setDate(M, D, Y);
-		setPhoneNumber(P);
-		setRelationship(R);
+		setFirstName(F);													//calls member function setFirstName
+		setLastName(L);														//calls member function setLastName
+		address.setAddress(A);												//calls the addressType member function setAddress
+		address.setCity(C);													//calls the addressType member function setCity
+		address.setState(S);												//calls the addressType member function setState
+		address.setZipcode(Z);												//calls the addressType member function setZipcode
+		birthday.setDate(M, D, Y);											//calls the dateType member function setDate
+		setPhoneNumber(P);													//calls the member function setPhoneNumber
+		setRelationship(R);													//calls the member function setRelationship
 	}
-	void setPhoneNumber(std::string N) {
-		phoneNumber = N;
+	void setPhoneNumber(std::string N) {									//uses one string as a formal parameter
+		phoneNumber = N;													//sets phoneNumber as the parameter N
+	}	
+	std::string getPhoneNumber() {											//has no formal parameters
+		return phoneNumber;													//returns the current value for phoneNumber
 	}
-	std::string getPhoneNumber() {
-		return phoneNumber;
-	}
-	void setRelationship(std::string R) {
-		if (R == "Family" || R == "Friend" || R == "Business") {
-			relationship = R;
+	void setRelationship(std::string R) {									//uses one string as a formal parameter
+		if (R == "Family" || R == "Friend" || R == "Business") {			//checks if R is "Family" "Friend" or "Business"
+			relationship = R;												//sets relationship as parameter R if true
 		}
-		else {
+		else {																//gives error message and sets relationship to default if false
 			std::cerr << "Invalid relationship, please enter Family, Friend, or Business." << std::endl;
 			relationship = "";
 		}
 	}
-	std::string getRelationship() {
-		return relationship;
+	std::string getRelationship() {											//has no formal parameters
+		return relationship;												//returns current value of relationship
 	}
-	int getBirthMonth() {
-		return birthday.getMonth();
+	int getBirthMonth() {													//has no formal parameters
+		return birthday.getMonth();											//returns the value of the dateType member function getMonth
 	}
-	void print() {
-		std::cerr << getFirstName() << " " << getLastName() << std::endl;
-		birthday.print();
-		address.print();
-		std::cerr << phoneNumber << std::endl << relationship << std::endl;
+	void print() {															//has no formal parameters
+		std::cerr << getFirstName() << " " << getLastName() << std::endl;	//prints firstName, lastName, endl
+		birthday.print();													//calls the dateType member function print
+		address.print();													//calls the addressType member function print
+		std::cerr << phoneNumber << std::endl << relationship << std::endl;	//prints phoneNumber, endl, relationship, endl
 
 	}
 private:
